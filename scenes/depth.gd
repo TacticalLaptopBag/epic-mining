337b4b77c31f299bf7fbf658173c1dd8e3df0fb3
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-06-25 08:42:39 -0400

    Removed debug print from depth

--- scenes/depth.gd
+++ scenes/depth.gd
@@ -11,5 +11,4 @@ func _physics_process(_delta: float):
 	var player_pos := Refs.player.position
 	var player_feet := player_pos.y - 1
 	var player_depth: int = round(max(-player_feet / block_size.y, 0))
-	print(str(-player_feet / block_size.y))
 	text = "Depth: "+str(player_depth)
