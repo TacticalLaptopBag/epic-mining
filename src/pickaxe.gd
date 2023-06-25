38e79526d7a4d85a69a8711ff6889e183e8c6381
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-06-25 16:24:19 -0400

    Added block selection

--- src/pickaxe.gd
+++ src/pickaxe.gd
@@ -9,6 +9,7 @@ const ANIM_TIME = 0.75
 var pick_ready := true
 var auto_repeat := false
 var current_block: MiningBlock = null
+var selected_block: MiningBlock = null
 var mining := false
 
 func _ready():
@@ -21,13 +22,14 @@ func _unhandled_input(_event: InputEvent):
 	mining = Input.is_action_pressed("mine") or current_block != null
 
 func _physics_process(_delta: float):
+	select_block()
 	if mining:
 		update_current_block()
 		mine_current_block()
 	
 	anim_player.speed_scale = PlayerStats.pickaxe_speed
 
-func update_current_block():
+func get_block() -> MiningBlock:
 	var mouse_pos := get_viewport().get_mouse_position()
 	var from := project_ray_origin(mouse_pos)
 	var to := from + project_ray_normal(mouse_pos) * pickaxe_range
@@ -38,9 +40,27 @@ func update_current_block():
 	var collider = result.get("collider")
 
 	if collider != null and collider.is_in_group("MiningBlock"):
-		if (current_block != null and current_block == collider) or current_block == null:
+		return collider
+
+	return null
+
+func select_block():
+	var look_block = get_block()
+	if look_block != selected_block:
+		if selected_block != null:
+			selected_block.on_deselected()
+		selected_block = look_block
+		if selected_block != null:
+			selected_block.on_selected()
+
+func update_current_block():
+	selected_block = get_block()
+
+	if selected_block != null:
+		if (current_block != null and current_block == selected_block) or current_block == null:
 			# Player is still looking at current block or started mining a new block
-			current_block = collider
+			current_block = selected_block
+			current_block.on_selected()
 		else:
 			# Cancel mining - player looked at another block
 			current_block = null
