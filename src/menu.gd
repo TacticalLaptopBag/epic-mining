0d2617e1f9e87c7926cf18518a9c21b920d75439
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-06-24 23:28:58 -0400

    Menu invisible on start

--- src/menu.gd
+++ src/menu.gd
@@ -4,3 +4,6 @@ func _input(_event: InputEvent):
 	if Input.is_action_just_pressed("menu"):
 		visible = !visible
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if visible else Input.MOUSE_MODE_CAPTURED
+
+func _enter_tree():
+	visible = false
