d69d52b511f17dd7935b3b57518a698fbe342dac
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-06-25 22:49:57 -0400

    Changed the loaded order of ore resources

--- src/refs.gd
+++ src/refs.gd
@@ -7,11 +7,12 @@ var ores := {}
 func _ready():
 	var ore_rscs := [
 		preload("res://ores/diamond.tres"),
-		preload("res://ores/dirt.tres"),
 		preload("res://ores/gold.tres"),
 		preload("res://ores/iron.tres"),
 		preload("res://ores/stone.tres"),
+		preload("res://ores/dirt.tres"),
 	]
+
 	for ore_rsc in ore_rscs:
 		print("Loaded Ore Resource: "+ore_rsc.display_name)
 		ores[ore_rsc.display_name] = ore_rsc
